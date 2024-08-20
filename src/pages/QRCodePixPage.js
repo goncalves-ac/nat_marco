0461caa42c988f9eed33dcc2fd55bcb45df2c0ea
goncalves-ac <acgoncalves2@outlook.com>
2024-08-19 23:23:47 -0300

    Updated

--- src/pages/QRCodePixPage.js
+++ src/pages/QRCodePixPage.js
@@ -7,12 +7,25 @@ import Countdown from './../component/Countdown.js';
 import QRCodePixIMG from './../img/qr-code.png';
 
 const QRCodePage = () => {
+
+  const copyPixToClipboard = () => {
+    const pixCode = '00020126360014br.gov.bcb.pix0114+55319880817965204000053039865802BR5924NATALIA PEREIRA DE SOUZA6014Belo Horizonte610930640-16062290525PJUO428988251724027278793630461BB';
+    navigator.clipboard.writeText(pixCode)
+      .then(() => {
+        alert('Código PIX copiado para a área de transferência!');
+      })
+      .catch((err) => {
+        console.error('Erro ao copiar o código PIX: ', err);
+      });
+  };
+
   return (
     <div className="qrcode-page">
         <NavBar />
         <Countdown targetDate="2025-01-05T00:00:00" />
         <div className="qrcode-container-page">
             <img src={QRCodePixIMG} alt="QRCode para o PIX de presente" className="qrcode-image-page" />
+            <button onClick={copyPixToClipboard} className="copy-pix-button">Copiar pix</button>
         </div>
     </div>
   );
