285e1869c690228d0406f075437a5be6df10f8c5
goncaves-ac <acgoncalves2@outlook.com>
2024-08-26 22:01:26 -0300

    PostIt

--- src/pages/PostItBoard.js
+++ src/pages/PostItBoard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import NavBar from './../component/NavBar.js';
 import Countdown from './../component/Countdown.js';
 import "./../style/PostItBoard.css";
