47b10a49a5d67ce3a77490809f32988888802223
Aspiraa <tulio99bh@hotmail.com>
2024-08-30 12:40:14 -0300

    conectando com banco

--- src/pages/RSVPFormPage.js
+++ src/pages/RSVPFormPage.js
@@ -7,22 +7,32 @@ function RSVPFormPage() {
   const [rsvps, setRSVPs] = useState([]);
 
   const addRSVP = async (newRSVP) => {
-    const response = await fetch(`${process.env.DB_HOST}/api/rsvps`, {
+    const response = await fetch(`${process.env.REACT_APP_API_URL}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(newRSVP),
+      body: JSON.stringify({
+        name: newRSVP.name,
+        conf: newRSVP.conf,
+        message: newRSVP.message,
+        readed: false,
+      }),
     });
+
     const data = await response.json();
-    setRSVPs((prevRSVPs) => [...prevRSVPs, data]);
+    if (data.status === 'success') {
+      setRSVPs((prevRSVPs) => [...prevRSVPs, newRSVP]);
+    } else {
+      console.error('Erro ao salvar RSVP:', data.message);
+    }
   };
 
   return (
     <section>
       <NavBar />
       <Countdown targetDate="2025-01-05T00:00:00" />
-      <div className='RSVP'>
+      <div className="RSVP">
         <RSVPForm setRSVPs={addRSVP} />
       </div>
     </section>
