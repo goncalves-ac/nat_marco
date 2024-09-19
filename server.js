5631b73ce586001d020aa51199626b99d64e3c31
aspiraa <tulio99bh@hotmail.com>
2024-09-19 10:07:05 -0300

    tentando arrumar banco

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const mysql = require('mysql');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-require('dotenv').config();  // Carrega o .env na raiz do site
+const path = require('path'); // Necessário para especificar caminhos
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') }); // Configura o dotenv para pegar o .env fora de public_html
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,9 +10,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Configure a conexão com o banco de dados
+// Conexão com o banco de dados usando as variáveis de ambiente do .env
+const mysql = require('mysql');
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
+  host: process.env.REACT_APP_API_URL,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
@@ -26,26 +27,16 @@ db.connect((err) => {
   console.log('Conectado ao banco de dados MySQL');
 });
 
-// Rota para fornecer a URL da API ao frontend
+// Rota para expor a URL da API ao frontend
 app.get('/api/config', (req, res) => {
   res.json({ apiUrl: process.env.REACT_APP_API_URL });
 });
 
-// Rota para obter todos os RSVPs
-app.get('/api/rsvps', (req, res) => {
-  db.query('SELECT * FROM rsvps', (err, results) => {
-    if (err) {
-      res.status(500).json({ error: err });
-      return;
-    }
-    res.json(results);
-  });
-});
-
-// Rota para adicionar um novo RSVP
+// Rota para lidar com RSVPs
 app.post('/api/rsvps', (req, res) => {
   const { name, conf, message } = req.body;
   const newRSVP = { name, conf, message };
+
   db.query('INSERT INTO rsvps SET ?', newRSVP, (err, results) => {
     if (err) {
       res.status(500).json({ error: err });
@@ -55,7 +46,6 @@ app.post('/api/rsvps', (req, res) => {
   });
 });
 
-// Inicia o servidor
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
--- src/pages/RSVPFormPage.js
+++ src/pages/RSVPFormPage.js
@@ -8,8 +8,12 @@ function RSVPFormPage() {
   const [apiUrl, setApiUrl] = useState('');
 
   useEffect(() => {
+    const dbHostEnv = process.env.REACT_APP_API_URL;
+    
+    // Logando no console para verificar se está acessando corretamente
+    console.log('DB_HOST:', dbHostEnv);
     // Buscar a URL da API através de uma rota do backend
-    fetch('/api/config')
+    fetch(dbHostEnv)
       .then((response) => response.json())
       .then((data) => {
         setApiUrl(data.apiUrl);  // Define a URL da API recebida do backend
